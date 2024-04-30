8a7adb83443f7ef8135cb77383d93f21df63ebc2
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-04-30 12:15:20 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb://127.0.0.1:27017/'
+    const url='mongodb+srv://akhil:akhil@cluster0.kzna9t8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     const dbname='shopping'
    
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
